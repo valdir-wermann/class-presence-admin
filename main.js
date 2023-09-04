38a88ab6189628ab067fe31841f38b903926973a
valdir-wermann <valdirwermannjr@gmail.com>
2023-09-04 20:22:32 -0300

    added code for student

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 const username = document.querySelector('#username');
 const password = document.querySelector('#password');
+const type = document.querySelector('#type');
 const createBtn = document.querySelector('#create');
 
 const create = () => {
     const body = {
         username: username.value,
-        password: password.value
+        password: password.value,
+        type: type.value
     };
 
     fetch(`https://class-presence-backend.onrender.com/api/teacher_code/create`, {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json',
-            'Authorization': localStorage.getItem('authorization')
+            'Content-Type': 'application/json'
         },
         body: JSON.stringify(body)
     })
@@ -23,7 +24,7 @@ const create = () => {
             }
             throw new Error(res);
         })
-        .then(code => alert(`O código para criação do professor é: ${code.code}`));
+        .then(code => alert(`O código para criação é: ${code.code}`));
 }
 
 createBtn.addEventListener('click', create);
